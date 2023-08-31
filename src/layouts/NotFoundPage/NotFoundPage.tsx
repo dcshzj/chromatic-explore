ca53172e214a719867b71da1a0c294c6763f2c99
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2023-08-31 17:05:55 +0800

    Update NotFoundPage.tsx

--- src/layouts/NotFoundPage/NotFoundPage.tsx
+++ src/layouts/NotFoundPage/NotFoundPage.tsx
@@ -38,7 +38,7 @@ export const NotFoundPage = (): JSX.Element => {
               fontWeight="medium"
               color="base.content.strong"
             >
-              The page you are looking for does not exist anymore RIP AAA.
+              The page you are looking for does not exist anymore.
             </Text>
             <Text textStyle="body-2" color="base.content.default">
               Try refreshing your page when you return.
@@ -49,7 +49,7 @@ export const NotFoundPage = (): JSX.Element => {
             to={siteName ? `/sites/${siteName}/workspace` : "/sites"}
             mb="8rem"
           >
-            Back to ALL SITES!!!
+            Back to homepage!!!
           </Button>
         </VStack>
       </GridItem>
