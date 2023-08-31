799d3f4ee8d6d2f09034f801fad14642d9140238
Hsu Zhong Jun <27919917+dcshzj@users.noreply.github.com>
2023-08-31 16:52:32 +0800

    Update SiteDashboard.tsx

--- src/layouts/SiteDashboard/SiteDashboard.tsx
+++ src/layouts/SiteDashboard/SiteDashboard.tsx
@@ -327,7 +327,7 @@ export const SiteDashboard = (): JSX.Element => {
                     Site settings
                   </DisplayCardTitle>
                   <DisplayCardCaption>
-                    Manage site footer, links, logos, and A LITTLE BIT more
+                    Manage site footer, links, logos, and JUST A LITTLE BIT more
                   </DisplayCardCaption>
                 </DisplayCardHeader>
               </DisplayCard>
